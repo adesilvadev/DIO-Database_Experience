79045edd1d435a5f0758297569312286e18fee88
adesilvadev <adesilva.dev@gmail.com>
2022-09-18 15:30:35 -0300

    Commit projeto Intellij

---
+++ src/br/com/dio/Programa_Intellij.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class Programa_Intellij {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+    }
+}
---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,66 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private string nome;
+    private string cor;
+    private string idade;
+
+    public Gato(string nome, string cor, string idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public Gato() {
+
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public string getCor() {
+        return cor;
+    }
+
+    public void setCor(string cor) {
+        this.cor = cor;
+    }
+
+    public string getIdade() {
+        return idade;
+    }
+
+    public void setIdade(string idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome=" + nome +
+                ", cor=" + cor +
+                ", idade=" + idade +
+                '}';
+    }
+}
+
